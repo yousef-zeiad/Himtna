5e86ea1a7460bfacd24e01752fd2a3f0e0810d20
H.Alawneh <halawneh595@gmail.com>
2022-03-21 00:47:41 +0300

    setup complete

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
-import { Provider } from 'react-redux';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
 import store from './store';
 import Navigation from './components/Navigation/Naigation';
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   useEffect(() => {
-    SplashScreen.hide()
-  }, [])
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
-      <StatusBar barStyle='light-content' />
+      <StatusBar barStyle="light-content" />
       <Navigation />
     </Provider>
   );
--- components/AuthLoading/AuthLoading.tsx
+++ components/AuthLoading/AuthLoading.tsx
@@ -1,27 +1,28 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Container, SafeView } from './styled';
-import { ActivityIndicator } from 'react-native';
+import React, {useEffect} from 'react';
+import {Container, SafeView} from './styled';
+import {ActivityIndicator} from 'react-native';
 import localStorage from '../helpers/localSorage';
 import actions from '../../actions';
-import { useDispatch } from 'react-redux'
+import {useDispatch} from 'react-redux';
 
-export default function AuthLoading({ navigation }) {
+export default function AuthLoading({navigation}) {
   const dispatch = useDispatch();
   useEffect(() => {
-    async function getTokens() {
+    (async function getTokens() {
       const token = await localStorage.get('token');
       if (token) {
-        dispatch(actions.auth.setTokens(token))
+        dispatch(actions.auth.setTokens(token));
         navigation.navigate('App');
       } else {
-        navigation.navigate('Auth');
+        navigation.navigate('App');
+        // navigation.navigate('Auth');
       }
-    };
-    getTokens();
+    })();
   }, []);
+
   return (
     <Container>
-      <SafeView forceInset={{ top: 'always' }}>
+      <SafeView forceInset={{top: 'always'}}>
         <ActivityIndicator />
       </SafeView>
     </Container>
